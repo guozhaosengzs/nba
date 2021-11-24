f8356fbff71e737a2d423a4523111de23a71c4cf
joycezhhhh <92232751+joycezhhhh@users.noreply.github.com>
2021-11-24 00:19:25 -0500

    Revert "Update routes.js"
    This reverts commit a6e6c154fa7854489fd2dbb8f3810ab4f0581414.

--- project_rubric/server/routes.js
+++ project_rubric/server/routes.js
@@ -20,104 +20,13 @@ connection.connect();
 
 // Route 1 (handler)
 async function game(req, res) {
-    //Return an array with required information of a specific game.
-    //Results should include 3 JSON objects:
-    //Team_Game_Info: Game_ID, Game_Date; Nickname, seasonal wins up to now, seasonal losses up to now, Points in game, ftm in game, fgm in game for both teams
-    //Team_Season_Stats: Average stats in this season: pts, ast, fg, pf for both teams
-    //Leading_player_Info : player with highest pts and the pts value, player with highest ast and the ast value, player with highest fg and the fg value, player with highest pf and the pf value  for both teams
-    if (isNaN(req.query.Game_ID)) {
-        res.writeHead(500, {'Error': 'Please pass parameter Game_ID'});
-    }
-    else{
-        const Game_ID = req.query.Game_ID;
-        connection.query(
-            `WITH Game_Info AS (SELECT Game_ID,Season_ID,Game_Date,T1.Nickname AS Nickname_Home, T2.Nickname AS Nickname_Away,Pts_Home,Pts_Away,Ftm_Home,Ftm_Away,Fgm_Home,Fgm_Away,Team_Abbreviation_Home AS HT ,Team_Abbreviation_Away AS AT
-                FROM Game Join Team T1 on Game.Team_Abbreviation_Home = T1.Abbreviation
-                          Join Team T2 on Game.Team_Abbreviation_Away = T2.Abbreviation
-                WHERE Game_ID = ${Game_ID}),         
-                HT_win_loss AS(SELECT HT1.HT AS Home_Team,HT1.HT_win_as_home+ HT2.HT_win_as_away AS Home_seasonal_wins, HT1.HT_lose_as_home + HT2.HT_lose_as_away AS Home_seasonal_losses
-                FROM
-                (SELECT g.Team_Abbreviation_Home AS HT, SUM(CASE WHEN WL_Home = 'W' Then 1 Else 0 End) AS HT_win_as_home, SUM(CASE WHEN WL_Home = 'L' Then 1 Else 0 End) AS HT_lose_as_home
-                FROM Game g JOIN Game_Info gi on g.Team_Abbreviation_Home = gi.HT
-                Where g.Season_ID = gi.season_Id AND g.Game_Date <= gi.Game_Date) AS HT1
-                NATURAL JOIN
-                (SELECT g.Team_Abbreviation_Away AS HT, SUM(CASE WHEN WL_Home = 'L' Then 1 Else 0 End) AS HT_win_as_away, SUM(CASE WHEN WL_Home = 'W' Then 1 Else 0 End) AS HT_lose_as_away
-                FROM Game g JOIN Game_Info gi on g.Team_Abbreviation_Away = gi.HT
-                Where g.Season_ID = gi.season_Id AND g.Game_Date < gi.Game_Date) AS HT2),
-                AT_win_loss AS(SELECT AT1.AT AS Away_Team, AT1.AT_win_as_home+ AT2.AT_win_as_away AS Away_seasonal_wins, AT1.AT_lose_as_home + AT2.AT_lose_as_away AS Away_seasonal_losses
-                FROM
-                (SELECT g.Team_Abbreviation_Home AS AT, SUM(CASE WHEN WL_Home = 'W' Then 1 Else 0 End) AS AT_win_as_home, SUM(CASE WHEN WL_Home = 'L' Then 1 Else 0 End) AS AT_lose_as_home
-                FROM Game g JOIN Game_Info gi on g.Team_Abbreviation_Home = gi.AT
-                Where g.Season_ID = gi.season_Id AND g.Game_Date < gi.Game_Date) AS AT1
-                NATURAL JOIN
-                (SELECT g.Team_Abbreviation_Away AS AT, SUM(CASE WHEN WL_Home = 'L' Then 1 Else 0 End) AS AT_win_as_away, SUM(CASE WHEN WL_Home = 'W' Then 1 Else 0 End) AS AT_lose_as_away
-                FROM Game g JOIN Game_Info gi on g.Team_Abbreviation_Away = gi.AT
-                Where g.Season_ID = gi.season_Id AND g.Game_Date <= gi.Game_Date) AS AT2)
-                Select Game_Id, Game_Date,Nickname_Home,Nickname_Away,Pts_Home,Pts_Away,Ftm_Home,Ftm_Away,Fgm_Home,Fgm_Away,Home_seasonal_wins,Home_seasonal_losses,Away_seasonal_wins,Away_seasonal_losses
-                FROM Game_Info,HT_win_loss,AT_win_loss;
-
-                
-                WITH Teams AS (SELECT Season_ID,Team_Abbreviation_Home AS HT ,Team_Abbreviation_Away AS AT
-                    FROM Game
-                    WHERE Game_ID = ${Game_ID}),
-                count_games_HT AS(SELECT count(*) AS num_games_HT
-                    FROM Game
-                    WHERE Season_ID IN (SELECT Season_ID FROM Teams) AND (Team_Abbreviation_Home IN (SELECT HT FROM Teams) OR Team_Abbreviation_Away IN (SELECT HT FROM Teams))),
-                count_games_AT AS(SELECT count(*) AS num_games_AT
-                    FROM Game
-                    WHERE Season_ID IN (SELECT Season_ID FROM Teams) AND (Team_Abbreviation_Home IN (SELECT AT FROM Teams) OR Team_Abbreviation_Away IN (SELECT AT FROM Teams))),
-                HT_stats AS(SELECT CAST(SUM(PTS)/num_games_HT AS DECIMAL(5,1)) AS Home_Season_Pts,CAST(SUM(AST)/num_games_HT AS DECIMAL(5,1)) AS Home_Season_Ast,CAST(SUM(FG)/num_games_HT AS DECIMAL(5,1))  AS Home_Season_FG, CAST(SUM(PF)/num_games_HT AS DECIMAL(5,1)) AS Home_Season_PF
-                    FROM Seasons_Stats,count_games_HT
-                    WHERE Year IN (SELECT Season_ID From Teams) and Tm = (SELECT HT from Teams)),
-                AT_stats AS (SELECT CAST(SUM(PTS)/num_games_AT AS DECIMAL(5,1)) AS Away_Season_Pts,CAST(SUM(AST)/num_games_AT AS DECIMAL(5,1)) AS Away_Season_Ast,CAST(SUM(FG)/num_games_AT AS DECIMAL(5,1))  AS Away_Season_FG, CAST(SUM(PF)/num_games_AT AS DECIMAL(5,1)) AS Away_Season_PF
-                    FROM Seasons_Stats,count_games_AT
-                    WHERE Year IN (SELECT Season_ID From Teams) and Tm = (SELECT AT from Teams))
-                SELECT Home_Season_Pts,Away_Season_Pts,Home_Season_FG,Away_Season_FG,Home_Season_Ast,Away_Season_Ast,Home_Season_PF,Away_Season_PF
-                FROM HT_stats,AT_stats；
-
-    
-                WITH Teams AS (SELECT Season_ID,Team_Abbreviation_Home AS HT ,Team_Abbreviation_Away AS AT
-                    FROM Game
-                    WHERE Game_ID = ${Game_ID}),
-                HT_player_ranks AS(SELECT Player,AVG(PTS/G) AS player_pts,AVG(AST/G) AS player_ast,AVG(PF/G) AS player_pf,AVG(FG/G) AS player_fg,
-                ROW_NUMBER() over (ORDER BY AVG(PTS/G) DESC) AS pts_rank,
-                ROW_NUMBER() over (ORDER BY AVG(AST/G) DESC) AS ast_rank,
-                ROW_NUMBER() over (ORDER BY AVG(PF/G) DESC) AS pf_rank,
-                ROW_NUMBER() over (ORDER BY AVG(FG/G) DESC) AS fg_rank
-                From Seasons_Stats
-                WHERE Year IN (Select Season_ID From Teams) AND (Tm IN (Select HT FROM Teams))
-                GROUP BY Player),
-                AT_player_ranks AS(SELECT Player,AVG(PTS/G) AS player_pts,AVG(AST/G) AS player_ast,AVG(PF/G) AS player_pf,AVG(FG/G) AS player_fg,
-                ROW_NUMBER() over (ORDER BY AVG(PTS/G) DESC) AS pts_rank,
-                ROW_NUMBER() over (ORDER BY AVG(AST/G) DESC) AS ast_rank,
-                ROW_NUMBER() over (ORDER BY AVG(PF/G) DESC) AS pf_rank,
-                ROW_NUMBER() over (ORDER BY AVG(FG/G) DESC) AS fg_rank
-                From Seasons_Stats
-                WHERE Year IN (Select Season_ID From Teams) AND (Tm IN (Select AT FROM Teams))
-                GROUP BY Player)
-                SELECT *
-                FROM (SELECT Player AS Home_PTS_king,Cast(player_pts AS DECIMAL(5,1)) AS Home_Highest_PTS FROM HT_player_ranks WHERE pts_rank = 1) ht1,
-                (SELECT Player AS Away_PTS_king,Cast(player_pts AS DECIMAL(5,1)) AS Away_Highest_PTS FROM AT_player_ranks WHERE pts_rank = 1) at1,
-                (SELECT Player AS Home_AST_king,Cast(player_ast AS DECIMAL(5,1)) AS Home_Highest_AST FROM HT_player_ranks WHERE ast_rank = 1) ht2,
-                (SELECT Player AS Away_AST_king,Cast(player_ast AS DECIMAL(5,1)) AS Away_Highest_AST FROM AT_player_ranks WHERE ast_rank = 1) at2,
-                (SELECT Player AS Home_PF_king,Cast(player_pf AS DECIMAL(5,1))  AS Home_Highest_PF  FROM HT_player_ranks WHERE pf_rank = 1) ht3,
-                (SELECT Player AS Away_PF_king,Cast(player_pf AS DECIMAL(5,1))  AS Away_Highest_PF  FROM AT_player_ranks WHERE pf_rank = 1) at3,
-                (SELECT Player AS Home_FG_king,Cast(player_fg AS DECIMAL(5,1)) AS Home_Highest_FG FROM HT_player_ranks WHERE fg_rank = 1) ht4,
-                (SELECT Player AS Away_FG_king,Cast(player_fg AS DECIMAL(5,1)) AS Away_Highest_FG FROM AT_player_ranks WHERE fg_rank = 1) at4
-
-                `, [1, 2, 3], function (error, results, fields) {
-                    if (error) {
-                        console.log(error)
-                        res.json({ error: error })
-                    } else if (results) {
-                        res.json({ results: 
-                            {'Game_Team_Info': results[0], 
-                             'Team_Season_Stats': results[1],
-                             'Leading_Player_Info': results[2]
-                         }})
-                    }
-                })
-    }
+    // returns the information of 1 specific game given gameID
+    //Results should include:
+    // Game_ID, Game_Date
+    // Nickname, seasonal wins up to now, seasonal losses up to now, Points in game, ftm in game, fgm in game for both teams
+    // Seasonal average stats in this season: pts, ast,fg,pf for both teams
+    // Seasonal leading players : player with highest pts, player with highest ast,player with highest fg,player with highest pf for both teams
+    // Query Parameter(s): Game_ID(int)
 }
 
 // Route 2 (handler)
