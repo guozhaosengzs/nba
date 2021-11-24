2e80a0bac5cb30abc5dd3415ca69c112e78f22e5
joycezhhhh <92232751+joycezhhhh@users.noreply.github.com>
2021-11-24 03:31:50 -0500

    Update server.js

--- project_rubric/server/server.js
+++ project_rubric/server/server.js
@@ -12,19 +12,21 @@ app.use(cors({
     origin: '*'
 }));
 
-//page game: route 1-2
+//page game: route 1-4
 app.get('/game', routes.game)
+app.get('/gameteam', routes.game_team_info)
+app.get('/gameplayer', routes.game_player_info)
 app.get('/search/games', routes.search_games)
 
-//page player: route 3-4
+//page player: route 5-6
 app.get('/players', routes.player)
 app.get('/search/players', routes.search_player)
 
-//page team: route 5-6
+//page team: route 7-8
 app.get('/search/team', routes.search_team)
 app.get('/team', routes.get_team)
 
-//page home: route 7-
+//page home: route 9-
 app.get('/player_avg', routes.player_avg)
 
 app.listen(config.server_port, () => {
