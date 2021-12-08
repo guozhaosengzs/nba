16aa1c1b9a496e40f32a4d18e1287f3405c0449b
GZS <guozhaosengzs@yahoo.com>
2021-12-07 20:19:25 -0500

    restarted new REACT APP
    clean slate, this is the base code after installation, nothing's added

--- project_rubric/client/src/index.js
+++ project_rubric/client/src/index.js
@@ -1,41 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-	BrowserRouter as Router,
-	Route,
-	Switch
-} from 'react-router-dom';
-
-import HomePage from './pages/HomePage';
-import PlayersPage from './pages/PlayersPage';
-import 'antd/dist/antd.css';
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "shards-ui/dist/css/shards.min.css"
-import MatchesPage from './pages/MatchesPage';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <div>
-    <Router>
-      <Switch>
-        <Route exact
-							path="/"
-							render={() => (
-								<HomePage />
-							)}/>
-        <Route exact
-							path="/players"
-							render={() => (
-								<PlayersPage />
-							)}/>
-        <Route exact
-							path="/matches"
-							render={() => (
-								<MatchesPage />
-							)}/>
-      </Switch>
-    </Router>
-  </div>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
